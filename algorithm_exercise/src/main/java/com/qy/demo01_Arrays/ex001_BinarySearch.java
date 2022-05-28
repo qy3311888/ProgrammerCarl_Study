ee95ef4993bbc85dc51010ab5c287f03ef371762
qy <qy3311888@163.com>
2022-05-29 03:23:18 +0800

    ProgrammerCarl_Study 2022/05/28

---
+++ algorithm_exercise/src/main/java/com/qy/demo01_Arrays/ex001_BinarySearch.java
@@ -0,0 +1,26 @@
+package com.qy.demo01_Arrays;
+
+/**
+ * 704. 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ */
+public class ex001_BinarySearch {
+    public int binarySearch(int[] nums, int target) {
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
